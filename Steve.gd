d81e0fe705cc9c820531760e7126286f4506a65c
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-09-27 21:59:00 +0200

    jump action

--- Steve.gd
+++ Steve.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 var velocity = Vector2(0,0)
 const SPEED = 180
+const GRAVITY = 30
+const JUMPFORCE = -900
 
 func _physics_process(_delta):
   if Input.is_action_pressed("right"):
@@ -9,8 +11,11 @@ func _physics_process(_delta):
   if Input.is_action_pressed("left"):
     velocity.x = -SPEED
 
-  velocity.y = velocity.y + 30
-  
+  velocity.y = velocity.y + GRAVITY
+
+  if Input.is_action_pressed("jump"):
+    velocity.y = JUMPFORCE
+
   velocity = move_and_slide(velocity)
 
   velocity.x = lerp(velocity.x, 0, 0.2)
