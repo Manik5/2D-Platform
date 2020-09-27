c75377c4e3955a58006b6f231e286d7017d6c209
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-09-27 22:08:48 +0200

    jump action

--- Steve.gd
+++ Steve.gd
@@ -13,9 +13,9 @@ func _physics_process(_delta):
 
   velocity.y = velocity.y + GRAVITY
 
-  if Input.is_action_pressed("jump"):
+  if Input.is_action_just_pressed("jump") and is_on_floor():
     velocity.y = JUMPFORCE
 
-  velocity = move_and_slide(velocity)
+  velocity = move_and_slide(velocity, Vector2.UP)
 
   velocity.x = lerp(velocity.x, 0, 0.2)
