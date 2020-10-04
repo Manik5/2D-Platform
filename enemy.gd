fc9db66356362af61d7eb48113028c434d947ef2
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-10-04 16:09:34 +0200

    adding enemies

--- enemy.gd
+++ enemy.gd
@@ -2,15 +2,17 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 export var direction = -1
+export var detects_cliffs = true
 
 func _ready():
 	if direction == 1:
 		$AnimatedSprite.flip_h = true
 	$floor_checker.position.x = $CollisionShape2D.shape.get_extents().x * direction
+	$floor_checker.enabled = detects_cliffs
 
 func _physics_process(delta):
 	
-	if is_on_wall():
+	if is_on_wall() or not $floor_checker.is_colliding() and detects_cliffs and is_on_floor():
 		direction = direction * -1
 		$AnimatedSprite.flip_h = not $AnimatedSprite.flip_h
 		$floor_checker.position.x = $CollisionShape2D.shape.get_extents().x * direction
