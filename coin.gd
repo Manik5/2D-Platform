f1207644aa1d5e09ccc61248124b9fa120a0cc8a
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-10-05 21:53:42 +0200

    sounds effect

--- coin.gd
+++ coin.gd
@@ -6,6 +6,7 @@ func _on_coin_body_entered(body):
 	$AnimationPlayer.play("bounce")
 	emit_signal("coin_collected")
 	set_collision_mask_bit(0, false)
+	$SoundCoinCollect.play()
 	
 func _on_AnimationPlayer_animation_finished(anim_name):
 	queue_free()
